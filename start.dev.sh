0b9b9fd5d50f768e48ee7ecac6d1679f5ad7d67a
Leonard-ThindPad-P50 <leonard@thinkpad-p50.com>
2026-07-01 13:50:59 +0700

    optimal service be for wsl

--- start.dev.sh
+++ start.dev.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
+# mkdir -p ~/projects
+# cp -r /mnt/d/01.WORKS/WWW/Thang_long_che_viet_project ~/projects/
+# cd ~/projects/Thang_long_che_viet_project
+
 docker compose down
-docker rm -f tl_che_viet
+docker rm -f $(docker ps -aq)
 docker compose -f docker-compose.yml up -d --build
 
 # skill task in cd.auto.sh after running
 # ./cd.auto.sh
 
-./cd.auto.sh stop
-./cd.auto.sh
+# ./cd.auto.sh stop
+# ./cd.auto.sh
